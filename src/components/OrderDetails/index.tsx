2e7aa366ef88815110e2c7cbf89f6bf5415aaf25
Charles Marques <charles_sjp@yahoo.com.br>
2022-08-02 10:30:58 -0300

    implement component orderlist and buttonToggle

---
+++ src/components/OrderDetails/index.tsx
@@ -0,0 +1,20 @@
+import ButtonToggle from "components/ButtonToggle";
+import * as S from "./style";
+
+const OrderDetails = () => {
+  return (
+    <S.OrderDetails>
+      <S.OrderDetailsTitle>Detalhes do Pedido</S.OrderDetailsTitle>
+      <S.OrderDetailsButtonGroup>
+        <ButtonToggle active={true} value="Comer no Local" />
+        <ButtonToggle active={false} value="P/ Viagem" />
+        <ButtonToggle active={true} value="Delivery" />
+      </S.OrderDetailsButtonGroup>
+      <S.OrderDetailsList>
+        <p>Itens do pedido</p>
+      </S.OrderDetailsList>
+    </S.OrderDetails>
+  );
+};
+
+export default OrderDetails;
