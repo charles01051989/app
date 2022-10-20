4033bca71fc98d84aedffb4e225fae6f7aeabb78
Charles Marques <charles_sjp@yahoo.com.br>
2022-10-20 15:49:58 -0300

    add manage products component

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
-import { RoutePath } from 'types/routes';
+import { Routes, Route } from "react-router-dom";
+import { RoutePath } from "types/routes";
 import Home from "pages/Home/index";
-import Login from 'pages/Login';
-import Settings from 'pages/Settings';
+import Login from "pages/Login";
+import Settings from "pages/Settings";
+import ManageProducts from "components/ManageProducts";
 
 const Router = () => {
-    return (
-        <Routes>
-            <Route path={RoutePath.LOGIN} element={<Login />} />
-            <Route path={RoutePath.HOME} element={<Settings />} />
-            {/* <Route path={RoutePath.SETTINGS} element={<Settings/>} /> */}
-        </Routes>
-    );
-}
+  return (
+    <Routes>
+      <Route path={RoutePath.LOGIN} element={<Login />} />
+      <Route path={RoutePath.HOME} element={<Settings />} />
+      <Route path={RoutePath.SETTINGS} element={<Settings />} />
+      <Route path={RoutePath.SETTINGS_PRODUCTS} element={<ManageProducts />} />
+    </Routes>
+  );
+};
 
 export default Router;
